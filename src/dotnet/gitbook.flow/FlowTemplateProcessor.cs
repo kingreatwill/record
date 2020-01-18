a8c4af0bd143f97871d970a3c92cce9e0cda20e8
kingreatwill <350840291@qq.com>
2020-01-18 17:30:06 +0800

    Razor

--- src/dotnet/gitbook.flow/FlowTemplateProcessor.cs
+++ src/dotnet/gitbook.flow/FlowTemplateProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using gitbook.core;
 using gitbook.core.middleware;
+using Microsoft.AspNetCore.Razor.Language;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,8 +24,12 @@ namespace gitbook.flow
         public async Task Invoke(MiddlewareContext context)
         {
             var output = Path.ChangeExtension(context.ItemPath.ToString(), ".html");
-            new FsPath(output).WriteFile("");
-            await _next(context);
+            // new FsPath(output).WriteFile("");
+            // https://github.com/dotnet/aspnetcore-tooling/blob/master/src/Razor/src/Microsoft.AspNetCore.Razor.Language/RazorEngineBuilderExtensions.cs
+
+            var sd = RazorProjectEngine.Create();
+            sd.Process();
+           await _next(context);
         }
     }
 }
